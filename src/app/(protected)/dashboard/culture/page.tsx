29360cfccf665a633c62cefa8d242538d7a5413e
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2026-02-09 21:41:03 -0300

    feat- adiciona o componente Culture.tsx no componente page.tsx da rota app da aplicação.

--- src/app/(protected)/dashboard/culture/page.tsx
+++ src/app/(protected)/dashboard/culture/page.tsx
@@ -1,11 +1,9 @@
+import Culture from "@/components/home-dashboard/culture/Culture";
+
 export default function GenericPage() {
   return (
     <div className="h-full flex flex-col items-center justify-center text-center py-20">
-      <div className="w-16 h-16 bg-gray-50 rounded-full flex items-center justify-center mb-4 text-2xl">
-        🚧
-      </div>
-      <h1 className="text-xl font-bold text-gray-900">Módulo em Construção</h1>
-      <p className="text-gray-500 max-w-xs">Este módulo estratégico está em desenvolvimento.</p>
+      <Culture />
     </div>
   );
 }
